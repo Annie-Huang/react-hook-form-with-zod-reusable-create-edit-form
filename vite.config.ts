61972ff885be51589b19a6e8888477002afed4f1
Annie Huang <annie.hn@gmail.com>
2024-10-15 21:57:49 +1100

    Update components.json, tsconfig.json, vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import Unfonts from 'unplugin-fonts/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
