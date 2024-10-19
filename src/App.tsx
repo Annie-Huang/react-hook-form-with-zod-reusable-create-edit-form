bd9ff0c5d02a220505276e06c47e8174f40e67e4
Annie Huang <annie.hn@gmail.com>
2024-10-19 16:50:55 +1100

    Pass in the user detail in teh <UserForm /> component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import UserForm from './components/UserForm.tsx';
 function App() {
   return (
     <div className='mx-auto max-w-[500px]'>
-      <UserForm />
+      <UserForm
+        user={{
+          id: 1,
+          birthday: new Date(),
+          firstName: 'Darius',
+          lastName: 'Cosden',
+          email: 'email@cosdensolutions.io',
+          role: 'viewer',
+        }}
+      />
     </div>
   );
 }
